570e16c63a3f6f969b7535b80eb1af75e9230930
Jina <yongjinc@bu.edu>
2020-11-17 23:37:38 -0500

    front and back setup

--- prototype/backAPI/config/fetchConfigs.js
+++
@@ -1,9 +0,0 @@
-
-const fetchOptions = {
-    method: 'GET',
-    my_client_id: '115bbc6789374858b5baf8b18df7584d',
-    client_id_secret: 'fe810ca819774ba5bd2614dd4e32b32f',
-    redirect_url: 'http://localhost:9000/callback',
-}
-
-module.exports = {fetchOptions}
