c5370cf95f95defd336912c0a0e81a7ac820c47d
Jina <yongjinc@bu.edu>
2020-11-22 09:04:26 -0500

    config

--- prototype/server/config/fetchConfigs.js
+++
@@ -1,8 +0,0 @@
-
-const fetchOptions = {
-    client_id : 'ff2fa4499ed0455ba2b78579db1b13de',
-    client_secret : 'f1a7b8253fb2493c8089e86287b06453',
-    redirect_uri : 'http://localhost:9000/callback'
-}
-
-module.exports = {fetchOptions}
