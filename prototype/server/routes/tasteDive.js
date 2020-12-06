62a80aa7b02f8810c64bee3e228d996dad668265
drash7 <43657207+drash7@users.noreply.github.com>
2020-12-06 21:40:25 +0530

    TasteDive data with required format for graph

--- prototype/server/routes/tasteDive.js
+++ prototype/server/routes/tasteDive.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const router = express.Router();
-const CONFIG = require('../config/fetchConfigs');
-const fetch = require('node-fetch');
+const CONFIG = require("../CONFIG/fetchConfigs");
 
 const access_key = CONFIG.tasteDive.access_key;
 
-//Need to retrieve artist from elsewhere, based on user's shared artist interests
-//const artist = graph.node.artist (pseudocode example of how the artist variable should operate)
+// const API  = `https://tastedive.com/api/similar?q=${artist}&info=1&k=${access_key}`
 
-const artist = "Billie Eilish";
+// this can be replaced with the list of artists sent from Spotify
+let artists = ["Billie Eilish", "Cardi B"];
 
-async function fetchData(){
-    fetch(`https://tastedive.com/api/similar?q=${artist}&info=1&k=${access_key}`)
-        .then(response => {
-            return response.json();
-        }).then(data => {
-        const x = data.Similar;
-        console.log(x);
+const axios = require("axios");
+
+async function getDataAPI(artists) {
+  function getData(artists) {
+    return artists.map(async (artist) => {
+      return{
+        id: artist,
+        associations:(await axios.get (
+          `https://tastedive.com/api/similar?q=${artist}&info=1&k=${access_key}`
+        )).data.Similar.Results.map(_ => _.Name)
+      } 
+      
     });
+  }
+ return (await Promise.all(getData(artists)))
 }
+getDataAPI(artists).then(data => console.log(data))
 
-fetchData();
-
-
-
-
-
-module.exports = router;
+/*
+ * Basically what we are doing in the getData(artists) function is returning an array of Promises, and each Promise 
+ * is a wrapper around an object of the form { id: "ArtistName", associations: [similar artists]  } 
+ * In the Higher Order Function getDataAPI(artists), we are calling the getData(artists), which as discovered 
+ * earlier, returns an array of Promises. Then, we make use of Promise.all(), which will resolve/reject each Promise in the array. After the 
+ * execution completes, it will give us our required data in an array. 
+ * Key Point though: An async function always returns a promise. getDataAPI is an async function, so it will return
+ * a promise. We can do a .then on it and get our data.
+ */
