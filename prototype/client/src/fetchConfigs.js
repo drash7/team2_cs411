e4037b2822e773641768e70abd503b339b13bdca
elissone <elissone@bu.edu>
2020-12-09 12:59:31 -0500

    Revert "git ignore"
    This reverts commit 30412eaa870336c0d8a4d4eec63285673fe672fb.

---
+++ prototype/client/src/fetchConfigs.js
@@ -0,0 +1,12 @@
+
+// client id and client secret stored here
+// git ignore this file!
+
+const fetchOptions = {
+    client_id: 'ff2fa4499ed0455ba2b78579db1b13de',
+    client_secret: '8d5cbf5cba994adcaeb05d528a72a1c1',
+    url:'http://localhost:9000/callback'
+}
+
+
+module.exports = {fetchOptions}
