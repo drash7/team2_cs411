6230b7459bb36d8468179b7376fbac61e4fa7880
drash7 <43657207+drash7@users.noreply.github.com>
2020-12-13 15:52:13 +0530

    improved description

--- prototype/client/src/Pages/About/Data.js
+++ prototype/client/src/Pages/About/Data.js
@@ -5,7 +5,7 @@ export const aboutObjOne = {
     lightTextDesc: true, 
     lightText: true, 
     topLine: 'About the Website', 
-    description: `MusicBridge helps you build a bridge between you and a friend's music taste. We use Spotify to find you and a friend's favourite artists in order to find and showcase common ground. This is done through an interactive graph that visualizes the intersection of your tastes and a playlist with tunes you might both like. Connect with a friend and happy bridging!`,
+    description: `MusicBridge helps you build a bridge between yours and a friend's music taste. We use Spotify to find you and your friend's favourite artists in order to identify and showcase common ground. This is done through an interactive graph that visualizes the intersection of your tastes and a playlist with tunes you both may like. Connect with a friend and happy bridging!`,
     headline : 'Introduction',
     img: require('../../assets/images/MusicBridgeLogo.png'), 
     start: '',
