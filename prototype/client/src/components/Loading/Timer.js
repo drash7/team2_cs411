642520b0241952d73df526ffeb13a422919919ea
drash7 <43657207+drash7@users.noreply.github.com>
2020-12-13 23:30:41 +0530

    timer created, not redirected to graph yet

--- prototype/client/src/components/Loading/Timer.js
+++ prototype/client/src/components/Loading/Timer.js
@@ -1,3 +1,42 @@
+import React, { Component } from 'react'
+
+class Timer extends Component   {
+    constructor(props)  {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+    render()    {
+        const {count} = this.state
+        return (
+            <div>
+                <h1>Loading: {count}</h1>
+            </div>
+        )
+    }
+
+    componentDidMount () {
+        const {startCount} = this.props
+        this.setState({
+            count: startCount
+        })
+        this.myInterval = setInterval(() => {
+            this.setState(prevState => ({
+                count: prevState.count - 1
+            }))
+        }, 1000)
+        
+    }
+
+    componentWillUnmount()  {
+        clearInterval(this.myInterval)
+    }
+    // render()
+}
+
+export default Timer
+
 // import React, { Component } from 'react'
 
 // const Timer = () => {
